42f906efccfe7e70d60a8a4379d53517f2d8e2a7
unknown <dirm2705@gmail.com>
2024-04-24 16:37:06 -0600

    Agregadas instrucciones de nivel

--- Frontend/screens.c
+++ Frontend/screens.c
@@ -1,9 +1,27 @@
 #include "frontend.h"
 
-//Pantalla principal
+// Pantalla principal
 
-//Instrucciones generales
+// Instrucciones generales
 
-//Instrucciones por nivel
+// Instrucciones por nivel
+void imprimir_instrucciones_nivel(int nivel)
+{
+    printf("NIVEL %d\n", nivel);
+    printf("En este nivel, te enfrentaras a:\n");
+    switch (nivel)
+    {
+        case 1:
+            printf("Los " BRIGHT_GREEN "PLANETAS CON VIDA " WHITE "que no pueden ser destruidos\n");
+            printf("y los " BRIGHT_BLUE "PLANETAS SIN VIDA " WHITE "que pueden ser destruidos.\n");
+            break;
+        case 2:
+            printf("Los " BLUE "ASTEROIDES\n");
+            break;
+        case 3:
+            printf("Los " PURPLE "HOYOS NEGROS\n");
+            break;
+    }
+}
 
-//Display
+// Display
