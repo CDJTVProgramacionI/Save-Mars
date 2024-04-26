9ada8fcb79786eee1484050be26b1ec0484dc0ef
DIRM2705 <dirm2705@gmail.com>
2024-04-26 08:22:00 -0600

    Añadidas estructuras

--- Logic/Structs/structs.c
+++ Logic/Structs/structs.c
@@ -1,1 +1,19 @@
 #include "structs.h"
+typedef struct
+{
+    int capsvid;
+    int misiles;
+    int velocidad;
+    int dist;
+} nave;
+
+typedef struct
+{
+    short int esObstaculo;
+    char* nombre;
+    int maxDist;
+    int misilesCorrecto;
+    int misilesIncorrecto;
+    int vidasCorrecto;
+    int vidasIncorrecto;
+} objeto;
---
+++ Logic/Structs/structs.h
@@ -0,0 +1,7 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct nave;
+typedef struct objeto;
+
+#endif
