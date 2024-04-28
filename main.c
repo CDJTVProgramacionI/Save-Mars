7ec59014ea54abf9b523ced30b7a9a8b214e1964
DIRM2705 <dirm2705@gmail.com>
2024-04-28 09:36:19 -0600

    Creada función main

--- main.c
+++ main.c
@@ -3,4 +3,37 @@
 #include "Logic/Levels/levels.h"
 #include "Logic/Utils/utils.h"
 #include "Logic/Structs/structs.h"
-#include <stdio.h>
+
+void main()
+{
+    // Declarar variables
+    int juego = 1;
+    char opc;
+
+    // Pantalla principal
+
+    // Menu
+
+    do
+    {
+        switch (opc)
+        {
+            case '1':
+                // Juego nuevo
+                break;
+            case '2':
+                // Imprimir última bitácora
+                break;
+            case '3':
+                // Imprimir historico de juegos
+                break;
+            case '4': // Salir
+                break;
+            default:
+                printf("Opción no válida");
+                break;
+        }
+
+        //Enter para borrar
+    } while (opc != '4');
+}
