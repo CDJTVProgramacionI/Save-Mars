086365ae117b2d6d264b6ff319390223019bd866
DIRM2705 <dirm2705@gmail.com>
2024-04-28 09:43:47 -0600

    Añadido manejo de errores

--- Data/reading.c
+++ Data/reading.c
@@ -2,26 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int get_number(FILE* file)
+int get_number(FILE *file)
 {
-    char* num;
+    char *num;
     int tam = 1;
     do
     {
-        num = (char*)realloc(num, tam * sizeof(char));
+        num = (char *)realloc(num, tam * sizeof(char));
+        if (num == NULL)
+        {
+            printf("Ha ocurrido un error");
+            exit(0);
+        }
         num[tam - 1] = fgetc(file);
         tam++;
     } while (!feof(file) && num[tam - 2] != ',');
     return atoi(num);
 }
 
-int* read_n_outputs(const char* dir, int output_size)
+int *read_n_outputs(const char *dir, int output_size)
 {
 
-    int* data = calloc(output_size, sizeof(int));
+    int *data = calloc(output_size, sizeof(int));
     FILE *file = fopen(dir, "r");
-    //leer números
-    for(int i = 0; i < output_size; i++)
+    if (data == NULL || file == NULL)
+    {
+        printf("Ha ocurrido un error");
+        exit(0);
+    }
+    // leer números
+    for (int i = 0; i < output_size; i++)
     {
         data[i] = get_number(file);
     }
