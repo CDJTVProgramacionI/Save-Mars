d93dab2280fb5cab8785bf35072eca0461eac854
DIRM2705 <dirm2705@gmail.com>
2024-04-28 09:48:41 -0600

    Documentado el código

--- Data/reading.c
+++ Data/reading.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Obtener un número
 int get_number(FILE *file)
 {
     char *num;
     int tam = 1;
+
     do
     {
+        //Añadir caracteres a arreglo dinámico
         num = (char *)realloc(num, tam * sizeof(char));
         if (num == NULL)
         {
@@ -16,21 +19,25 @@ int get_number(FILE *file)
         }
         num[tam - 1] = fgetc(file);
         tam++;
-    } while (!feof(file) && num[tam - 2] != ',');
+    } while (!feof(file) && num[tam - 2] != ','); //Hasta fin de archivo o encontrar una coma
     return atoi(num);
 }
 
+//Leer del archivo la cantidad de entradas especificadas
 int *read_n_outputs(const char *dir, int output_size)
 {
 
     int *data = calloc(output_size, sizeof(int));
     FILE *file = fopen(dir, "r");
+
+    //Verificar que no hayan errores
     if (data == NULL || file == NULL)
     {
         printf("Ha ocurrido un error");
         exit(0);
     }
-    // leer números
+
+    // Leer números
     for (int i = 0; i < output_size; i++)
     {
         data[i] = get_number(file);
