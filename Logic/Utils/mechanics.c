9d248ff81a2aa3e2cfa35de9045580df77b84190
DIRM2705 <dirm2705@gmail.com>
2024-04-29 18:32:37 -0600

    mechanics

--- Logic/Utils/mechanics.c
+++ Logic/Utils/mechanics.c
@@ -1,21 +1,29 @@
 #include "utils.h"
-//Mecánica de velocidad
+// Mecánica de velocidad
+int velocidad(int distancia, int a, int b)
+{
+    int vel = 0;
+    // Si ya avanzó 100 km, variar la velocidad aleatoriamente
+    if (distancia >= 100)
+        vel = (rand() % a + b) * 1000;
 
-//Generar obstáculos aleatorios
+    return vel;
+}
+// Generar obstáculos aleatorios
 
-//Generar distancias
-int distanciazar()   
+// Generar distancias
+int distanciazar()
 {
     int distobst[7];
-    for (int i =0;i<7;i++)
+    for (int i = 0; i < 7; i++)
     {
-        if(i==0)
+        if (i == 0)
         {
-            distobst[0]=(rand()%8)+5;
+            distobst[0] = (rand() % 8) + 5;
         }
-        else 
+        else
         {
-            distobst[i]=(rand()%8)+(distobst[i-1])+1;
+            distobst[i] = (rand() % 8) + (distobst[i - 1]) + 1;
         }
     }
 
