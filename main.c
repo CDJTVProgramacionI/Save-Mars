2bcefa0bc1da9125cb132cf921524e4346dbebbf
DIRM2705 <dirm2705@gmail.com>
2024-05-04 09:14:15 -0600

    Creado imprimir histórico

--- main.c
+++ main.c
@@ -8,16 +8,39 @@
 void imprimirUltimaBitacora()
 {
     FILE *bitacora = fopen(BINNACLE_DIR, "r");
-    printf("----------------- BITÁCORA -----------------------");
+    printf("----------------- BITÁCORA -----------------------\n");
     while (!feof(bitacora))
     {
-        printf("**********************************************");
+        printf("**********************************************\n");
         int* data = read_n_outputs(bitacora, BINNACLE_OUTPUTS);
-        printf("Nivel: %d, turno: %d", data[2], data[3]);
-        printf("Distancia alcanzada: %d", data[0]);
-        printf("Velocidad del turno; %d", data[1]);
+        printf("Nivel: %d, turno: %d\n", data[2], data[3]);
+        printf("Distancia alcanzada: %d\n", data[0]);
+        printf("Velocidad del turno: %d\n", data[1]);
     }
     fclose(bitacora);    
+}
+
+void imprimirHistorico()
+{
+    FILE *archivo = fopen(RESULT_DIR, "r");
+    int juegos = 1;
+    printf("----------------- HISTÓRICO DE JUEGOS -----------------------\n");
+    while(!feof(archivo))
+    {
+        printf("*********************************************************\n");
+        int* data = read_n_outputs(archivo, RESULT_OUTPUTS);
+        printf("Juego: %d\n", juegos);
+        printf("Capsulas de vida finales: %d\n", data[0]);
+        printf("Misiles finales: %d\n", data[1]);
+        printf("Objetos de interés capturados: %d\n", data[2]);
+        printf("Resultado del juego: %s\n", data[2] == 1 ? "Ganó" : "Perdió");
+        if(juegos%5 == 0)
+        {
+            entercontinuar();
+        }
+        juegos++;
+    }
+    fclose(archivo); 
 }
 
 void main()
